2e411364331fc3ec4e38bd61d05a0e03a954f797
Ali Zain <alizain.python.programmer@protonmail.com>
2026-01-02 21:36:24 +0500

    chore(android): set applicationId to com.example.cryptoalert and rename JS component to CryptoAlert; update package files and app name

---
+++ android/app/src/main/java/com/example/cryptoalert/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cryptoalert;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "CryptoAlert";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        DefaultNewArchitectureEntryPoint.getFabricEnabled(),
+        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled()
+        );
+  }
+}
